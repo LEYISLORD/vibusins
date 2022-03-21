69aea0d539659617406ae0cb164dc966efcdca45
LEYISLORD <85986097+LEYISLORD@users.noreply.github.com>
2022-03-21 08:33:49 -0500

    Add files via upload

---
+++ functions/menu.js
@@ -0,0 +1,107 @@
+﻿exports.menu = (v) => {
+  return `┌─⊷ *MAIN* 
+▢ ${v}menuvc
+▢ ${v}bot <text>
+▢ ${v}info
+▢ ${v}language
+▢ ${v}ping
+▢ ${v}owner
+▢ ${v}join <link>
+▢ ${v}report <text>
+▢ ${v}listprem
+▢ ${v}listban
+▢ ${v}chatbot on/off
+└─────────────
+≡ List Menu
+┌─⊷ *FUN* 
+▢ ${v}fake text|@user|text
+▢ ${v}preg
+└─────────────
+┌─⊷ *GAMES* 
+▢ ${v}verdad
+▢ ${v}reto
+▢ ${v}ppt
+└─────────────
+┌─⊷ *STICKER* 
+▢ ${v}sticker <nombre>|<autor>
+▢ ${v}take <nombre>|<autor>
+▢ ${v}toimg
+▢ ${v}attp└─────────────
+┌─⊷ *TOOLS*
+▢ ${v}afk <razon>
+▢ ${v}fakethumb
+▢ ${v}tomp3
+▢ ${v}toav
+▢ ${v}q
+▢ ${v}say <text>
+
+
+
+
+└─────────────
+┌─⊷ *VOTING* 
+▢ ${v}voting <text>
+▢ ${v}delvote
+▢ ${v}checkvote
+└─────────────
+┌─⊷ *ADMIN* 
+▢ ${v}setwelcome
+▢ ${v}setbye
+▢ ${v}delwelcome
+▢ ${v}delbye
+▢ ${v}simulate <welcome/bye>
+▢ ${v}group <open/close>
+▢ ${v}resetlink
+▢ ${v}banchat
+▢ ${v}unbanchat
+▢ ${v}hidetag
+▢ ${v}notify
+▢ ${v}tagall
+▢ ${v}setppgp
+▢ ${v}setname <text>
+▢ ${v}setdesc <text>
+▢ ${v}add <numero>
+▢ ${v}kick @user
+▢ ${v}promote @user
+▢ ${v}demote @user
+▢ ${v}warn @user
+▢ ${v}delwarn @user
+▢ ${v}warns @user
+└─────────────
+┌─⊷ *GRUPO*
+▢ ${v}welcome on/off
+▢ ${v}antidelete on/off
+▢ ${v}detect on/off
+▢ ${v}antilink on/off
+▢ ${v}link
+▢ ${v}infogp
+▢ ${v}profile
+▢ ${v}invite <519xxxxxx> 
+└─────────────
+┌─⊷ *OWNER* 
+▢ ${v}addpremium @user
+▢ ${v}delpremium @user
+▢ ${v}ban @user
+▢ ${v}unban @user
+▢ ${v}view
+▢ ${v}grouplist
+▢ ${v}update
+▢ ${v}setppbot
+▢ ${v}setfakethumb
+▢ ${v}setprefix
+▢ ${v}listreport
+▢ $
+▢ >
+└─────────────`;
+};
+
+exports.menuVC = `
+┌─⊷ *MAIN* 
+▢ menu
+└─────────────
+┌─⊷ *BUSQUEDA*
+▢ google
+└─────────────
+┌─⊷ *DOWNLOAD* 
+▢ play
+└─────────────`;
